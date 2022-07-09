00bec9a11f0e931b7aabb1bf96073a0da5683f87
testrella68 <103074206+testrella68@users.noreply.github.com>
2022-07-09 16:37:11 -0400

    Add files via upload

---
+++ FirstBank/src/com/FirstBank.java
@@ -0,0 +1,17 @@
+package com;
+
+public class FirstBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Account mike = new Account("Mike", "A12345");
+		
+		
+		Account tiff = new Account("Tiff", "B54321");
+		tiff.showMenu();
+		
+		
+	}
+
+}
